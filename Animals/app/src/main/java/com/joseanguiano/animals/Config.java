718194703344b7eab5b07e0de890fbd95e2d3236
Jose Anguiano <c.joseanguiano@gmail.com>
2016-06-22 12:49:15 -0400

    Update Config.java

--- Animals/app/src/main/java/com/joseanguiano/animals/Config.java
+++ Animals/app/src/main/java/com/joseanguiano/animals/Config.java
@@ -4,6 +4,6 @@ package com.joseanguiano.animals;
  * Created by JoseAnguiano on 18/06/2016.
  */
 public class Config {
-    public static final String EMAIL ="c.joseanguiano@gmail.com";
-    public static final String PASSWORD ="AURC930808HVZNMR07";
+    public static final String EMAIL ="Ingresa tu correo";
+    public static final String PASSWORD ="Ingresa tu contraseña";
 }
